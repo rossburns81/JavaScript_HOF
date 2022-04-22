850ce085eab1c7ae4d51c0ec572793b8a665cc15
Ross Burns <101517539+rossburns81@users.noreply.github.com>
2022-04-22 12:13:27 -0500

    Completed

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+const plus = (num) => {
+    return (plusNumber) => num + plusNumber;
+};
+
+const plus15 = plus(15);
+
+console.log(plus15(10));
